99662f0a15d47ca91bbe59364989767c471c344e
madzia89 <wojcicka.ma@gmail.com>
2018-07-23 13:42:44 +0200

    add router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import React, {Component} from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import ListOfTurbines from './components/ListOfTurbines'
 import ListOfCars from './components/ListOfCars'
+import Dashboard from './components/Dashboard'
+
 
 class App extends Component {
     render() {
         return (
             <div>
-                <ListOfTurbines/>
-                <ListOfCars/>
+                <Router>
+
+                    <div>
+
+                        <Route
+                            exact path={'/'}
+                            component={() => (
+                                <Dashboard/>)}
+                        />
+
+                        <Route
+                            path={'/turbines'}
+                            component={() => (
+                                <ListOfTurbines/>)}
+                        />
+
+                        <Route
+                            path={'/cars'}
+                            component={() => (
+                                <ListOfCars/>)}
+                        />
+
+                    </div>
+
+                </Router>
             </div>
         )
     }
