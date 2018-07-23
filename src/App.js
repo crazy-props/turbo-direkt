a49c8fa2db4a7082666141b0dd67a56c43f28939
madzia89 <wojcicka.ma@gmail.com>
2018-07-23 10:58:52 +0200

    app init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+
+        <p>
+          turbo-project
+        </p>
+      </div>
+    )
+  }
+}
+
+export default App
