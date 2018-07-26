8c78756e9a7df05ea3bca9c692eedca9ef6135c3
Pawel Wasil <wasil.paul@gmail.com>
2018-07-26 13:02:53 +0200

    Revert "Material Ui added to project"
    This reverts commit e347a58d82324d000d9a1c50d661fe1e4b8b9cae.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { MuiThemeProvider } from 'material-ui'
-import { Provider } from 'react-redux'
-import { store } from './store'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux'
+import {store} from './store'
 
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
     <Provider store={store}>
-        <MuiThemeProvider>
-            <App />
-        </MuiThemeProvider>
+        <App/>
     </Provider>,
     document.getElementById('root')
 )
