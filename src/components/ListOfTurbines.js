64e4629d73b73b82d5f025a42b737e4a7009d6aa
Pawel Wasil <wasil.paul@gmail.com>
2018-08-27 14:16:29 +0200

    Code refactor

--- src/components/ListOfTurbines.js
+++ src/components/ListOfTurbines.js
@@ -3,16 +3,9 @@ import { connect } from 'react-redux'
 import style from '../style'
 import PartsColumn from './ListOfTurbiness_SingleView'
 
-/*
-const IngredientsList = ({ list }) =>
-	React.createElement('tr', null,
-		list.map((ingredient, i) =>
-			React.createElement('th', { key: i }, ingredient)
-		)
-	)
-const Ingredients = React.createFactory(IngredientsList)
-const list = ['Turbo OEM', 'Compressor Wheel', 'Turbine Wheel', 'Bearing Housing', 'Back Plate', 'Heat Shield', 'Actuator', 'Noozles', 'Gasket Kit', 'Repair Kit', 'KODE CHRA']
-*/
+const _listOfParts = ['Turbo OEM', 'Compressor Wheel', 'Turbine Wheel', 'Bearing Housing', 'Back Plate', 'Heat Shield', 'Actuator', 'Noozles', 'Gasket Kit', 'Repair Kit', 'KODE CHRA']
+const _parts = ['compressor_wheel', 'turbine_wheel', 'bearing_housing', 'back_plate', 'heat_shield', 'nozzles', 'actuator', 'gasket_kit', 'repair_kit', 'KODE_CHRA']
+
 class ListOfTurbines extends Component {
    render() {
       return this.props.turbo === null && this.props.part === null ?
@@ -20,19 +13,8 @@ class ListOfTurbines extends Component {
          :
          <table>
             <thead >
-               {/* {Ingredients({ list })} */}
                <tr style={style.table_head} >
-                  <th>Turbo OEM</th>
-                  <th>Compressor Wheel</th>
-                  <th>Turbine Wheel</th>
-                  <th>Bearing Housing</th>
-                  <th>Back Plate</th>
-                  <th>Heat Shield</th>
-                  <th>Actuator</th>
-                  <th>Noozles</th>
-                  <th>Gasket Kit</th>
-                  <th>Repair Kit</th>
-                  <th>KODE CHRA</th>
+                  {_listOfParts.map(el => <th>{el}</th>)}
                </tr>
             </thead>
             <tbody >
@@ -40,16 +22,7 @@ class ListOfTurbines extends Component {
                   x =>
                      <tr style={style.table_body}>
                         <td>{x.turboOEM}</td>
-                        <PartsColumn parts={x.compressor_wheel} />
-                        <PartsColumn parts={x.turbine_wheel} />
-                        <PartsColumn parts={x.bearing_housing} />
-                        <PartsColumn parts={x.back_plate} />
-                        <PartsColumn parts={x.heat_shield} />
-                        <PartsColumn parts={x.nozzles} />
-                        <PartsColumn parts={x.actuator} />
-                        <PartsColumn parts={x.gasket_kit} />
-                        <PartsColumn parts={x.repair_kit} />
-                        <PartsColumn parts={x.KODE_CHRA} />
+                        {_parts.map(part => <PartsColumn parts={x[part]} />)}
                      </tr>
                )}
             </tbody>
