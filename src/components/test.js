76789fd4523d566876cf9d771d0cf369fd351d40
vizzroot <vizzroot@gmail.com>
2018-08-27 21:31:12 +0200

    wop4

--- src/components/test.js
+++ src/components/test.js
@@ -2,6 +2,8 @@ import React from 'react'
 import {db} from '../firebase'
 import SingleTurbine from "./SingleTurbine";
 import Spinner from "./spinner"
+import {Col, Row} from "react-flexbox-grid";
+import TextField from "material-ui/TextField";
 
 var equal="";
 var content=[];
@@ -54,49 +56,68 @@ class Count extends React.Component {
     }
 
     render() {
-        let filter=this.state.list.filter(el=>el.mark.toLowerCase().indexOf(this.state.search)!==-1
+        let filter=this.state.list.filter(el=>el.mark.toLowerCase().indexOf(this.state.search)!==-1||el.mark.indexOf(this.state.searchTerm) !== -1
     ||el.turbo_OEM.toString().indexOf(this.state.search)!==-1
         );
         return (
-            <table className="table table-striped">
-                <thead>
-                <tr>
-                    <th className="filterable-cell">Mark</th>
-                    <th className="filterable-cell">Model</th>
-                    <th className="filterable-cell">Date</th>
-                    <th className="filterable-cell">Capacity</th>
-                    <th className="filterable-cell">No.</th>
-                    <th className="filterable-cell">Power</th>
-                    <th className="filterable-cell">Turbo OEM</th>
-                </tr>
-                <input type='text' onChange={this.updateSearch} value={this.state.search}/>
-                <button onClick={this.Search}>Update</button>
-                </thead>
-                <tbody>
-                {filter.map((el) =>
-                    <tr>
-                        <td>{el.mark}</td>
-                        <td>{el.model}</td>
-                        <td>{el.date}</td>
-                        <td>{el.capacity}</td>
-                        <td>{el.no}</td>
-                        <td>{el.power}</td>
-                        <td>
-                            <tr>
-                                {el.turbo_OEM && el.turbo_OEM.length ?
-                                    el.turbo_OEM.filter(function (a, b, c) {
-                                        return c.indexOf(a) === b;
-                                    }).map(el => <a href="http://localhost:3000/turbines"><SingleTurbine
-                                        el={el}/></a>
-                                    ) :
-                                    el.turbo_OEM
-                                } </tr>
-                        </td>
-                    </tr>
-                )
-                }
-                </tbody>
-            </table>
+            <div style={{marginTop:"15vh"}}>
+                <Row middle={'xs'} className={'partsSearchRow2'}>
+                    <Col xs={6}>
+                        <Row end={'xs'}>
+                            <Col xs={6}>
+                                <TextField
+                                    style={{margin: 'auto'}}
+                                    fullWidth={true}
+                                    id={'idForTextField'}
+                                    floatingLabelText={'Search for cars or turbochargers'}
+                                    type={"text"}
+                                    value={this.state.search}
+                                    onChange={(event => {
+                                        this.setState({search: event.target.value})
+                                    })}
+                                />
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+                <Row className={'partsTableDiv'}>
+                    <table className="carsTable">
+                        <thead className="carsTableHead">
+                        <th>Mark</th>
+                        <th>Model</th>
+                        <th>Date</th>
+                        <th>Capacity</th>
+                        <th>No.</th>
+                        <th>Power</th>
+                        <th className="lastTh">Turbo OEM</th>
+                        </thead>
+                        <tbody key={Math.random()}>
+                        {filter && filter.length ? filter.map((el) =>
+                            <tr className="trOne">
+                                <td>{el.mark}</td>
+                                <td>{el.model}</td>
+                                <td>{el.date}</td>
+                                <td>{el.capacity}</td>
+                                <td>{el.no}</td>
+                                <td>{el.power}</td>
+                                <td className="turboList">
+                                    {el.turbo_OEM && el.turbo_OEM.length ?
+                                        el.turbo_OEM.filter(function (a, b, c) {
+                                            return c.indexOf(a) === b;
+                                        }).map(el => <a
+                                            href="http://localhost:3000/turbines"><SingleTurbine
+                                            el={el}/></a>
+                                        ) :
+                                        el.turbo_OEM
+                                    }
+                                </td>
+                            </tr>
+                        ):<Spinner/>
+                        }
+                        </tbody>
+                    </table>
+                </Row>
+            </div>
         )
     }
 }
