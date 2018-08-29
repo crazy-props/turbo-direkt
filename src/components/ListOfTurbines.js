41f630af0556596dbdcb47b6920621ab3c598f5c
Pawel Wasil <wasil.paul@gmail.com>
2018-08-29 12:33:28 +0200

    Search for turbine component added

--- src/components/ListOfTurbines.js
+++ src/components/ListOfTurbines.js
@@ -2,33 +2,48 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import style from '../style'
 import PartsColumn from './ListOfTurbiness_SingleView'
+import SearchInput from './SearchInput';
 
 
 class ListOfTurbines extends Component {
    state = {
       _listOfParts: ['Turbo OEM', 'Compressor Wheel', 'Turbine Wheel', 'Bearing Housing', 'Back Plate', 'Heat Shield', 'Actuator', 'Noozles', 'Gasket Kit', 'Repair Kit', 'KODE CHRA'],
-      _parts: ['compressor_wheel', 'turbine_wheel', 'bearing_housing', 'back_plate', 'heat_shield', 'nozzles', 'actuator', 'gasket_kit', 'repair_kit', 'KODE_CHRA']
+      _parts: ['compressor_wheel', 'turbine_wheel', 'bearing_housing', 'back_plate', 'heat_shield', 'nozzles', 'actuator', 'gasket_kit', 'repair_kit', 'KODE_CHRA'],
+      turbineName: '',
    }
+
+   handleTurbineNameChangeChandler = (e, value) => this.setState({ turbineName: value });
+
    render() {
+
+      const listOfTurbines = this.props.turbo && this.props.turbo
+         .filter(nam => nam.turboOEM.toLowerCase().indexOf(this.state.turbineName.toLowerCase()) !== -1)
+
       return this.props.turbo === null && this.props.part === null ?
          <span>Loading .... </span>
          :
-         <table>
-            <thead >
-               <tr style={style.table_head} >
-                  {this.state._listOfParts.map(el => <th>{el}</th>)}
-               </tr>
-            </thead>
-            <tbody >
-               {this.props.turbo.map(
-                  x =>
-                     <tr style={style.table_body}>
-                        <td>{x.turboOEM}</td>
-                        {this.state._parts.map(part => <PartsColumn parts={x[part]} />)}
-                     </tr>
-               )}
-            </tbody>
-         </table>
+         <div>
+            <SearchInput
+               handleTurbineNameChangeChandler={this.handleTurbineNameChangeChandler}
+            />
+            <table>
+               <thead >
+                  <tr style={style.table_head} >
+                     {this.state._listOfParts.map(el => <th>{el}</th>)}
+                  </tr>
+               </thead>
+               <tbody >
+                  {listOfTurbines.map(
+                     turbine =>
+                        <tr>
+                           <td>{turbine.turboOEM}</td>
+                           {this.state._parts.map(part => <PartsColumn parts={turbine[part]} />)}
+                        </tr>
+                  )}
+
+               </tbody>
+            </table>
+         </div>
    }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux'
-import {store} from './store'
+import { Provider } from 'react-redux'
+import { store } from './store'
 
 import './index.css';
 import App from './App';
 import FormLoginOnStartup from "./components/FormLoginOnStartup";
+import  MuiThemeProvider  from 'material-ui/styles/MuiThemeProvider';
 
 ReactDOM.render(
     <Provider store={store}>
-        <App/>
+        <MuiThemeProvider>
+            <App />
+        </MuiThemeProvider>
     </Provider>,
     document.getElementById('root')
 )
