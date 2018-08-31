dfd04f80bbed2f19b2b8c7ea111e90f20426215a
vizzroot <vizzroot@gmail.com>
2018-08-31 11:40:40 +0200

    supermodel

--- src/components/SingleTurbine.js
+++ src/components/SingleTurbine.js
@@ -9,8 +9,6 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 
-
-
 const customContentStyle = {
     width: '100%',
     maxWidth: 'none',
@@ -49,7 +47,7 @@ class SingleTurbine extends React.Component {
             return (
                 <div >
                         <Dialog
-                            title={props.turbine}
+                            title={`TURBO OEM:${props.turbine}`}
                             actions={actions}
                             modal={true}
                             contentStyle={customContentStyle}
@@ -65,7 +63,7 @@ class SingleTurbine extends React.Component {
                                     //get all values and compare to part state (value: part )
                                     .map(part => props.part.filter(x => x.part === part))
                                     //show results (part name + part amount)
-                                    .map(x => x.map(val => `${val.part}: ${val.amount}`)
+                                    .map(x => x.map(val => `$(val.group) - ${val.part}: ${val.amount}`)
                             ).map(el=>
                                 <Table>
                                     <TableBody>
