e21416a8fb7e2670aa4ed00057906b654a6339ef
Pawel Wasil <wasil.paul@gmail.com>
2018-09-03 21:27:14 +0200

    Select field - decomposition state, one object to push create (el. CreateListOfParts)

--- src/components/CreteListOfParts.js
+++ src/components/CreteListOfParts.js
@@ -7,53 +7,65 @@ import { connect } from 'react-redux'
  * if the number of items causes the height to exceed this limit.
  */
 class CreateListOfParts extends Component {
-    state = {
-        _parts: ['compressor_wheel', 'turbine_wheel', 'bearing_housing', 'back_plate', 'heat_shield', 'nozzles', 'actuator', 'gasket_kit', 'repair_kit', 'KODE_CHRA'],
-        compressor_wheel: '',
-        turbine_wheel: '',
-        bearing_housing: '',
-        back_plate: '',
-        heat_shield: '',
-        nozzle: '',
-        actuator: '',
-        gasket_kit: '',
-        repair_kit: '',
-        KODE_CHRA: '',
-    };
+	state = {
+		_parts: ['compressor_wheel', 'turbine_wheel', 'bearing_housing', 'back_plate', 'heat_shield', 'nozzles', 'actuator', 'gasket_kit', 'repair_kit', 'KODE_CHRA'],
+		push: {
+			compressor_wheel: [],
+			turbine_wheel: [],
+			bearing_housing: [],
+			back_plate: [],
+			heat_shield: [],
+			nozzle: [],
+			actuator: [],
+			gasket_kit: [],
+			repair_kit: [],
+			KODE_CHRA: [],
+		}
+	};
 
-    /**
-    * 'handleChange' function is require to mapping all state _parts to each value handle function. 
-    * Name value is convert to object key property.
-    */
+   /**
+   * 'handleChange' function is require to mapping all state _parts to each value handle function. 
+   * Name value is convert to object key property. 
+   */
 
-    handleChange = name => (event, index, value) => { this.setState({ [name]: value }); }
+	handleChange = name => (event, index, value) => {
+		this.setState({ push: { ...this.state.push, [name]: [...this.state.push[name], value] } })
+	}
 
-    render() {
-        console.log(this.state)
-        return (this.props.part && this.props.part.length ?
-            this.state._parts.map(singlePart =>
-                <div>
-                    {`${singlePart}: `}
-                    <SelectField
-                        value={this.state[singlePart]}
-                        onChange={this.handleChange(singlePart)}
-                    >
-                        {
-                            this.props.part
-                                .filter(part =>
-                                    part.group === singlePart)
-                                .map(part =>
-                                    <MenuItem value={part.part} key={part.part} primaryText={`${part.part}`} />)
-                        }
-                    </SelectField>
-                </div>)
-            : 'Load'
-        )
-    }
+	render() {
+		console.log(this.state.push)
+		return (this.props.part && this.props.part.length ?
+			this.state._parts.map(singlePart =>
+				<div>
+					{`${singlePart}: `}
+					<SelectField
+						key={this.state.push[singlePart]}
+						value={this.state.push[singlePart]}
+						onChange={this.handleChange(singlePart)}
+					>
+						{
+							this.props.part
+								.filter(part =>
+									part.group === singlePart)
+								.map(part =>
+									<MenuItem
+										value={part.part}
+										key={part.part}
+										primaryText={`${part.part}`} />)
+						}
+					</SelectField>
+					{this.state.push[singlePart] && this.state.push[singlePart].length > 0 ?
+						this.state.push[singlePart].map(x => <span>{x} </span>)
+						:
+						`--------`}
+				</div>)
+			: 'Load'
+		)
+	}
 }
 
 const mapStateToProps = state => ({
-    part: state.partsState.parts,
+	part: state.partsState.parts,
 })
 
 const mapDispatchToProps = dispatch => ({
@@ -61,6 +73,6 @@ const mapDispatchToProps = dispatch => ({
 })
 
 export default connect(
-    mapStateToProps,
-    mapDispatchToProps
+	mapStateToProps,
+	mapDispatchToProps
 )(CreateListOfParts)
